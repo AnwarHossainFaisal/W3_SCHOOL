c14e9e80e661641b4f9b6e0c5b4be59506326ee5
Fahim <info.anwarh.52@gmail.com>
2026-01-17 17:35:59 +0600

    added python all variable name

---
+++ variable.py
@@ -0,0 +1,88 @@
+# creating variables
+a = "5 "
+b = "jhon"
+print(a + b)
+
+# output:5 jhon
+
+
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+a = 4
+a = "sally"
+print(a)
+# op: sally
+
+# type casting
+a = str(5)
+b = int(5)
+c = float(5)
+print(a, b, c)
+
+
+# Get the Type
+x = 5
+y = "jhon"
+
+print(type(x))
+print(type(y))
+
+# op:<class 'int'>
+# <class 'str'>
+
+# Single or Double Quotes?
+
+a = "jhon"
+a = 'Jhon'
+print(a)
+
+# Case-Sensitive
+# Variable names are case-sensitive.
+a = 3
+A = 5
+# op:3
+    # 5
+
+# Variable names are case-sensitive.
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+# Rules for Python variables:
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+
+# wright
+
+myvar = "jhon"
+my_var = "jhon"
+_my_var = "jhon"
+myVer = "jhon"
+MYVER = "jhon"
+myver2 = "jhon"
+
+
+# woring
+# 2myvar = "jhon"
+# my-ver = "jhon"
+# my ver = "jhon"
+
+
+# Multi Words Variable Names
+# Camel Case
+myVariavleName = 'jhon'
+
+
+# pascal Case
+Myvariablename = 'jhon'
+
+
+# snake case
+my_variavle_name = 'jhon'
+
+
+
+
+
