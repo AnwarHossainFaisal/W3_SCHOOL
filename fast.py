e0be62d6564c534358ed216849899c02ba84c509
Fahim <info.anwarh.52@gmail.com>
2026-01-17 12:18:17 +0600

    added python comment

--- fast.py
+++ fast.py
@@ -1,4 +1,4 @@
-print("hello world")
+# print("hello world")
 # Python Install
 
 
@@ -8,17 +8,53 @@ print("hello world")
 
 # Python Version
 
-import sys
+# import sys
 
-print(sys.version)
+# print(sys.version)
 
 # C:\Users\Your Name>python
 
 # C:\Users\Your Name>py
 
 # Execute Python Syntax
-print("Hello, World")
+# print("Hello, World")
 
 # C:\Users\Your Name>python myfile.py
+
+
+# Python Indentation
+
+# if 5 > 2:
+#     print("Five is gater then wwo")
+    
+
+# SyntaxError
+
+# if 5 > 2:
+# print("five is gater then two") 
+
+# if 5 > 2:
+#     print("five is gatter then two")
+
+# if 5 > 2:
+#         print("five is greater then two")
+
+
+
+# SyntaxError
+# if 5 > 2:
+#     print("five is greater then two")
+#         print("five is greater then two"
+
+
+
+# Python Variables
+x = 5 
+y = "hello world"
+print(x)
+print(y)
+
+# Comments
+# single comment
 
 
