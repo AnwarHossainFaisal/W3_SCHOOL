c55665d933e01594797f52f84b5dabe7def20fae
Fahim <info.anwarh.52@gmail.com>
2026-01-20 16:52:22 +0600

    added python fast data type

---
+++ dataType.py
@@ -0,0 +1,27 @@
+# Built-in Data Types
+# In programming, data type is an important concept.
+
+# Variables can store data of different types, and different types can do different things.
+
+# Python has the following data types built-in by default, in these categories:
+
+
+
+# 1. text Type: string
+# 2. Numeric Types: int, float, complex
+# 3. Sequence Types: list, tuple, range 
+# 4. mapping types: dict 
+# 5. set types: set, frozenset 
+# 6. Boolean Types: bool> True, False
+# 7. Binary Types: byte, bytearray, memoryvies
+# 8. None Type: None type
+
+
+
+x = "Hello"
+print(type(x))
+# op: "str"
+
+
+
+
