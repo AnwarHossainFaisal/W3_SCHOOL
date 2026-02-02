43f1c6bbf83eccc801ca751065e161e6b2b6c949
Fahim <info.anwarh.52@gmail.com>
2026-02-02 18:39:24 +0600

    added python modify string

--- string.py
+++ string.py
@@ -80,3 +80,134 @@ if "free" in txt:
 # Yes, 'free' is present
 
 
+
+
+# Check if NOT
+txt = "the best thing il life are free"
+print("expensive" not in txt)
+
+# op:
+True
+
+
+txt = "the best things in life are free"
+if "expensive" not in txt:
+    print("No,'expensive' is NOT Present. ")
+
+# op:
+# No,'expensive' is NOT Present. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python - Slicing Strings
+# Python - Slicing Strings
+# Python - Slicing Strings
+
+# slicing
+b = "Hello, World"
+print(b[2:5])
+
+# op:
+# llo
+
+
+
+
+# Slice From the Start
+a = "Hello, World"
+print(a[:5])
+# op:
+# Hello
+
+
+
+b = "Hello World"
+print(b[2:])
+
+# op:
+# llo World
+
+
+
+
+# Negative Indexing
+b = "Hello World"
+print(b[-5:-2])
+# op:
+# wor
+
+
+
+
+
+# Python - Modify Strings
+
+# Upper Case
+a = "Hello world"
+print(a.upper())
+
+# op:
+# HELLO WORLD
+
+a = "Hello World"
+print(a.lower())
+
+# op:
+# hello world
+
+
+
+
+# Remove Whitespace
+a = " Hello     world "
+print(a.strip())
+
+
+
+
+# Replace String
+a = "Hello World"
+print(a.replace("H", "J"))
+
+# op:
+# Jello World
+
+
+
+# Split String
+# split 
+
+a = "Hello, World"
+print(a.split(","))
+
+# op:
+# ['Hello', ' World']
+
+
+
+
+
+
+
+
+
+
+
+
+# Python - String Concatenation
+
+
+
+
+
