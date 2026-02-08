6a8ee3cf711416e34e0fa15f4bfea66f221a6b6a
Fahim <info.anwarh.52@gmail.com>
2026-02-08 22:37:56 +0600

    added python string count

--- stringmathod.py
+++ stringmathod.py
@@ -6,3 +6,57 @@ print(a)
 # op:
 # Hello, and welcoeme to python
 
+
+txt = "python is fun"
+a = txt.capitalize()
+print(a)
+# op:
+# Python is fun
+
+
+
+
+
+
+
+# Python String casefold() Method
+txt = "Hello, And welcome To Python"
+a = txt.casefold()
+a = txt.lower()
+print(a)
+# op:
+# hello, and welcome to python
+
+
+
+# Python String center() Method
+txt = "banana"
+a = txt.center(30)
+print(a)
+# op:
+            # banana
+
+
+
+# Parameter Values
+# txt = "banana"
+# a = txt.enter(10, 0)
+# print(a)
+# # op:
+
+
+
+# Python String count() Method
+txt = "i laove apple, apple ar my favorite fruit"
+a = txt.count("apple")
+print(a)
+# op: 2
+
+
+# Syntax
+# string.count(value, start, end)
+
+txt = "i love apple, apple is my favorite fruit"
+a = txt.count("apple", 10, 23)
+print(a)
+
