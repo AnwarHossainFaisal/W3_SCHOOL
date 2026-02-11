5be0bb38c1c2e18acee98fe1bdf8083daa7088f2
Fahim <info.anwarh.52@gmail.com>
2026-02-11 14:17:10 +0600

    added python operator

---
+++ operator.py
@@ -0,0 +1,58 @@
+print(10 + 5)
+
+sum1 = 100 + 50
+sum2 = sum1 + 250
+sum3 = sum1 + sum2
+
+print(sum1)
+print(sum2)
+print(sum3)
+# op:
+# 150
+# 400
+# 550
+
+
+# Python Arithmetic Operators
+# Arithmetic Operators
+
+# +, -, *, /, %, **, //
+
+
+# Examples
+a = 15
+b = 5
+
+print(a + b) 
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+# op:
+# 20
+# 10
+# 75
+# 3.0
+# 0
+# 759375
+# 3
+
+
+# Division in Python
+a = 33
+b = 5
+
+print(a / b)
+# op:
+# 6.6
+
+
+a = 33
+b = 5
+print(a // b)
+
+
+
+
